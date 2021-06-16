270a212d8c41a96974b70cd75f2a99dd9ae19055
Chris Chase <cchase@redhat.com>
2021-06-16 19:45:49 -0400

    Updated gh version

--- install.sh
+++ install.sh
@@ -6,27 +6,29 @@ BIN_DIR="$HOME/.local/bin"
 INSTALL_TMP="/tmp/notebook-setup"
 
 echo 'Installing gitconfig'
-wget -O "${HOME}/.gitconfig" https://raw.githubusercontent.com/cfchase/notebook-setup/master/.gitconfig &> /dev/null
+wget -O "${HOME}/.gitconfig" https://raw.githubusercontent.com/cfchase/notebook-setup/master/.gitconfig
 
 echo 'Installing cookiecutter package'
-pip install cookiecutter &> /dev/null
+pip install cookiecutter
 
 echo 'Installing GitHub client'
 mkdir -p "${INSTALL_TMP}"
+mkdir -p "${BIN_DIR}/"
 cd "${INSTALL_TMP}" || exit
-wget https://github.com/cli/cli/releases/download/v1.8.1/gh_1.8.1_linux_amd64.tar.gz &> /dev/null
-tar -xvf gh_1.8.1_linux_amd64.tar.gz &> /dev/null
-mv gh_1.8.1_linux_amd64/bin/gh ${BIN_DIR} &> /dev/null
+GH_VERSION='1.11.0'
+wget "https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.tar.gz"
+tar -xvf "gh_${GH_VERSION}_linux_amd64.tar.gz"
+mv "gh_${GH_VERSION}_linux_amd64/bin/gh" ${BIN_DIR}
 
 echo 'Installing OpenShift CLI (oc)'
 mkdir -p "${INSTALL_TMP}"
 cd "${INSTALL_TMP}" || exit
-wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz &> /dev/null
-tar -xvf openshift-client-linux.tar.gz &> /dev/null
-mv oc /opt/app-root/bin/oc &> /dev/null
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
+tar -xvf openshift-client-linux.tar.gz
+mv oc /opt/app-root/bin/oc
 
 
 echo 'Cleaning up'
-rm -rf "${INSTALL_TMP}"
+ rm -rf "${INSTALL_TMP}"
 
 popd || exit
