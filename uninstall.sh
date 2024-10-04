7e8a4998d93d6b455e2f081d1d6ef781339cc640
Christopher Chase <cchase@redhat.com>
2024-10-04 11:16:58 -0400

    remove uninstall of cookiecutter

--- uninstall.sh
+++ uninstall.sh
@@ -7,9 +7,6 @@ ODH_DIR="$HOME/.local/bin"
 echo 'Uninstalling odh client'
 rm "${ODH_DIR}/odh"
 
-echo 'Uninstalling cookiecutter package'
-pip uninstall cookiecutter -y &> /dev/null
-
 echo 'Uninstalling GitHub client'
 rm "${ODH_DIR}/gh"
 
